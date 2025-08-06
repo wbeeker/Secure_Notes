af7dc1b414d1f93d1026a79fb1fe8b37959f30de
wbeeker <willbeeker@gmail.com>
2025-08-06 08:24:53 -0700

    Initial Sring Boot + Gradle setup.

---
+++ src/test/java/com/example/secure_notes/SecureNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secure_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
