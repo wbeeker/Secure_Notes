c41392a96adcae327037c0532abb40d342b3a5bb
wbeeker <willbeeker@gmail.com>
2025-08-26 15:05:40 -0700

    Made SignupRequet

--- src/main/java/com/example/secure_notes/dto/SignupRequest.java
+++ src/main/java/com/example/secure_notes/dto/SignupRequest.java
@@ -1,5 +1,30 @@
 package com.example.secure_notes.dto;
 
 public class SignupRequest {
-    
+    private String username;
+    private String password;
+
+    public SignupRequest() {}
+
+    public SignupRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
