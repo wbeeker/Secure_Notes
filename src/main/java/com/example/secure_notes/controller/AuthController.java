3ce7353c83d0f9b8268a4f54cf44b77b0a856c61
wbeeker <willbeeker@gmail.com>
2025-08-27 12:40:37 -0700

    Built AuthController.

--- src/main/java/com/example/secure_notes/controller/AuthController.java
+++ src/main/java/com/example/secure_notes/controller/AuthController.java
@@ -1,5 +1,35 @@
 package com.example.secure_notes.controller;
 
+import com.example.secure_notes.dto.LoginRequest;
+import com.example.secure_notes.dto.SignupRequest;
+import com.example.secure_notes.dto.JwtResponse;
+import com.example.secure_notes.service.AuthService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<JwtResponse> signup(@RequestBody SignupRequest signupRequest) {
+        JwtResponse response = authService.signup(signupRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest loginRequest) {
+        JwtResponse response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
     
 }
