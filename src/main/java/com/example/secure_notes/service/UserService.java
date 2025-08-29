3a22012492784165780c460de9d8f5e408bc8c3b
wbeeker <willbeeker@gmail.com>
2025-08-29 14:22:35 -0700

    Created UserService file.

---
+++ src/main/java/com/example/secure_notes/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.secure_notes.service;
+
+import com.example.secure_notes.entity.User;
+import com.example.secure_notes.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+    
+}
