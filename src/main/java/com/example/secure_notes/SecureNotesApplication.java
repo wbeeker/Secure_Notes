e0509175c70755f36caf29898aac48c4fa54c9d2
wbeeker <willbeeker@gmail.com>
2025-10-30 12:05:01 -0400

    Added documentation.

--- src/main/java/com/example/secure_notes/SecureNotesApplication.java
+++ src/main/java/com/example/secure_notes/SecureNotesApplication.java
@@ -3,6 +3,9 @@ package com.example.secure_notes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Main entry point for the Secure Notes application.
+ */
 @SpringBootApplication(scanBasePackages = {"com.example.secure_notes"})
 public class SecureNotesApplication {
 	public static void main(String[] args) {
--- src/main/java/com/example/secure_notes/controller/HealthController.java
+++ src/main/java/com/example/secure_notes/controller/HealthController.java
@@ -3,9 +3,11 @@ package com.example.secure_notes.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Simple test controller for checking status of /api/ endpoints. 
+ */
 @RestController
 public class HealthController {
-
     @GetMapping("/api/health")
     public String health() {
         return "Server is running!";
